5c90221f5301e9023e37f212ae063a345a7be44f
Omniscient <omniscient.oce@gmail.com>
2024-03-02 15:31:29 +1100

    get stub generation working now that we have floats in bindgen

--- examples/basic/main.ml
+++ examples/basic/main.ml
@@ -1,1 +1,7 @@
-let () = print_endline "I dream of Genie!"
+let () =
+  print_endline "I dream of Genie!\n";
+  let x_axis : Djinn.vec2f = { x = 1.; y = 0. } in
+  Printf.printf "Here is a vector:\n\tX axis: (%f, %f)\n" x_axis.x x_axis.y;
+  let position : Djinn.vec2i = { x = 100; y = 150 } in
+  Printf.printf "We can draw shapes at some position, p: (%d, %d)\n"
+    position.x position.y
